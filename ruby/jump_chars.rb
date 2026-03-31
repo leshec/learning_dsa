95c61570f5033e3afb28fb4ae55770682d1eb453
leshec <none>
2026-03-31 23:59:36 +0100

    jump_chars with a tests

--- ruby/jump_chars.rb
+++ ruby/jump_chars.rb
@@ -1,7 +1,30 @@
 #I needed something like this instead of ifs. 
 #Had a bug elsewhere and though it failed due to modulo error
 
+#original with if condition, instead of mod
+def repeat_char_jump2(input_string, step)
+  # TODO: Use a loop to traverse the string with the given step, wrapping around when necessary.
+  result = ''
+  pointer = 0
+  seen = []
+
+  input_string.length.times do |i|
+    return input_string if input_string.length <= 1
+    seen << pointer 
+  result << input_string[pointer]
+  if (pointer + step) < input_string.length
+    pointer += step
+  else 
+    pointer = (pointer + step) - input_string.length 
+  end
+  if seen.include?(pointer)
+    pointer += 1
+  end
+end
+  result
+end
 # If length is 7 and pointer is 5, jumping by 3:
+
 #pointer = (5 + 3) % 7 # result is 1
 
 
@@ -43,9 +66,16 @@ def test_4
   assert_equal "cxgdlvd", repeat_char_jump("cgldxdv", 4)
 end
 
+
+def test_5
+  assert_equal "cxgdlvd", repeat_char_jump2("cgldxdv", 4)
+end
+
 puts "--- Running Manual Checks ---"
 puts repeat_char_jump('abcdefg', 3) # "adgcfbe"
 puts repeat_char_jump('abcd', 2)    # "acbd"
+puts repeat_char_jump('z', 1)  # "z"
 
 puts "\n--- Running Test Suite ---"
 test_4
+test_5
