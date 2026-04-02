65601a67ffbbdd753a8cc3ba2363f22e5cc189ac
Richard5 <richardmcewan100@gmail.com>
2026-04-02 18:12:34 +0200

    added some tests to help memorize

--- ruby/4_pair_elements_put_in_tuple.rb
+++ ruby/4_pair_elements_put_in_tuple.rb
@@ -19,3 +19,42 @@ def pair_elements(numbers)
   end
   answer_array
 end
+
+
+# This is not exhaustive.
+# Only one element etc
+# Pointers here go out from middle, my brain still goes out to in. Watch that. 
+def assert_equal(expected, actual)
+  if expected == actual
+    puts "PASS: got #{actual}"
+  else
+    puts "FAIL: got #{actual}, expected #{expected}"
+  end
+end
+
+def test_1 
+  assert_equal [[1,2],[1,2]], pair_elements([1,1,2,2])
+end
+
+def test_2
+  assert_equal [[1,2],[1,2],[1,2,]], pair_elements([1,1,1,2,2,2])
+end
+
+def test_3
+  assert_equal [[3,4],[2,5],[1,6]], pair_elements([1,2,3,4,5,6])
+end
+
+def test_4
+  assert_equal [[7,0], [6,8], [5,9]],     pair_elements([5,6,7,8,9])
+end
+
+puts "---Running Manual tests----"
+puts pair_elements([1,1,2,2]).inspect
+puts pair_elements([1,1,1,2,2,2]).inspect
+puts pair_elements([1,2,3,4,5,6]).inspect
+puts pair_elements([1,2,3]).inspect
+
+puts "---Running Defined Tests---"
+test_1
+test_2
+test_3
