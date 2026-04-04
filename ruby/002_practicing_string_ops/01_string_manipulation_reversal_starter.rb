dfcf88fd42e1c92994cf3c545b16f0460629eaf7
Richard5 <richardmcewan100@gmail.com>
2026-04-04 18:56:08 +0200

    done

--- ruby/002_practicing_string_ops/01_string_manipulation_reversal_starter.rb
+++ ruby/002_practicing_string_ops/01_string_manipulation_reversal_starter.rb
@@ -1,5 +1,10 @@
 def reverse_chars_and_sentence(s)
-puts "Yo"
+  answer = ''
+  words = s.split(' ')
+  words.each do |word|
+    answer << word.reverse + ' '
+  end
+  answer.strip
 end
 
 
@@ -11,8 +16,9 @@ def assert_equal(expected, actual)
   end
 end
 
+#There was a bug in their description Error:"..321_stisybur..". Fixed below. 
 def test_1
-  assert_equal "olleH taen 321_stisybur", reverse_chars_and_sentence("Hello neat rubyists_123")
+  assert_equal "olleH taen 321_stsiybur", reverse_chars_and_sentence("Hello neat rubyists_123")
 end
 
 puts "---Running manual---"
