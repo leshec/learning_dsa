fd7df0cc827888e6bce52b045a7cf2ffe0a22e88
Richard5 <richardmcewan100@gmail.com>
2026-04-05 11:37:04 +0200

    todo

---
+++ ruby/unit2/swap_words.rb
@@ -0,0 +1,15 @@
+def swap_words(s)
+  # TODO: Split the input string by hyphens, convert each word between numbers and letters as specified,
+  # and rejoin the results with hyphens.
+end
+
+def assert_equal(expected, actual)
+  if expected == actual
+    puts "Pass: got '#{actual}'"
+  else
+    puts "Fail: got '#{actual}', expected '#{expected}'"
+  end
+end
+
+
+
