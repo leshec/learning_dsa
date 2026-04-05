e15c4ec4d3c38d7b4bec391744d62d5fa33920b0
Richard5 <richardmcewan100@gmail.com>
2026-04-05 10:06:29 +0200

    tidy

--- ruby/002_practicing_string_ops/01_capitalize_words.rb
+++ ruby/002_practicing_string_ops/01_capitalize_words.rb
@@ -12,94 +12,66 @@ def assert_equal(expected, actual)
   end
 end
 
- def test_1
-    assert_equal 'Hello World', capitalize_words("hello world")
-  end
+def test_1
+  assert_equal 'Hello World', capitalize_words("hello world")
+end
 
-  def test_2
-    assert_equal 'Hello World', capitalize_words("HELLO WORLD")
-  end
+def test_2
+  assert_equal 'Hello World', capitalize_words("HELLO WORLD")
+end
 
-  def test_3
-    assert_equal '123 Hello', capitalize_words("123 hello")
-  end
+def test_3
+  assert_equal '123 Hello', capitalize_words("123 hello")
+end
 
-  def test_4
-    assert_equal '_underscore', capitalize_words("_underscore")
-  end
+def test_4
+  assert_equal '_underscore', capitalize_words("_underscore")
+end
 
-  def test_5
-    assert_equal 'First Second Third Fourth Fifth Sixth Seventh Eights Ninth Tenth',
-                 capitalize_words("first second third fourth fifth sixth seventh eights ninth tenth")
-  end
+def test_5
+  assert_equal 'First Second Third Fourth Fifth Sixth Seventh Eights Ninth Tenth',
+               capitalize_words("first second third fourth fifth sixth seventh eights ninth tenth")
+end
 
-  def test_6
-    assert_equal 'Single', capitalize_words("single")
-  end
+def test_6
+  assert_equal 'Single', capitalize_words("single")
+end
 
-  def test_7
-    assert_equal 'Hello Neat Ruby_123', capitalize_words("Hello neat ruby_123")
-  end
+def test_7
+  assert_equal 'Hello Neat Ruby_123', capitalize_words("Hello neat ruby_123")
+end
 
-  def test_8
-    assert_equal 'Some Random _text', capitalize_words("SoME rAndoM _TeXT")
-  end
+def test_8
+  assert_equal 'Some Random _text', capitalize_words("SoME rAndoM _TeXT")
+end
 
-  def test_9
-    assert_equal 'Caps Lock Is On', capitalize_words("CAPS lock IS on")
-  end
+def test_9
+  assert_equal 'Caps Lock Is On', capitalize_words("CAPS lock IS on")
+end
 
-  def test_10
-    assert_equal 'Mixed Case Sample', capitalize_words("mIxEd CaSe sample")
-  end
-
+def test_10
+  assert_equal 'Mixed Case Sample', capitalize_words("mIxEd CaSe sample")
+end
 
 puts "---manual checks----" 
-    puts capitalize_words("hello world")
+puts capitalize_words("hello world")
+puts capitalize_words("HELLO WORLD")
+puts capitalize_words("123 hello")
+puts capitalize_words("_underscore")
+puts capitalize_words("first second third fourth fifth sixth seventh eights ninth tenth")
+puts capitalize_words("single")
+puts capitalize_words("Hello neat ruby_123")
+puts capitalize_words("SoME rAndoM _TeXT")
+puts capitalize_words("CAPS lock IS on")
   
-
-  
-    puts capitalize_words("HELLO WORLD")
-  
-
-  
-    puts capitalize_words("123 hello")
-  
-
-  
-    puts capitalize_words("_underscore")
-  
-
-  
-    puts capitalize_words("first second third fourth fifth sixth seventh eights ninth tenth")
-  
-
-  
-    puts capitalize_words("single")
-  
-
-  
-    puts capitalize_words("Hello neat ruby_123")
-  
-
-  
-    puts capitalize_words("SoME rAndoM _TeXT")
-  
-
-  
-    puts capitalize_words("CAPS lock IS on")
-  
-
-    puts capitalize_words("mIxEd CaSe sample")
-  
-    puts "---tests---"
-  test_1
-  test_2
-  test_3
-  test_4
-  test_5
-  test_6
-  test_7
-  test_8
-  test_9
-  test_10
+puts "---tests---"
+test_1
+test_2
+test_3
+test_4
+test_5
+test_6
+test_7
+test_8
+test_9
+test_10
