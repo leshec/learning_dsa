ecca0026a4244765da475570a6b3e520b5264783
leshec <103497129+leshec@users.noreply.github.com>
2026-04-17 18:41:37 +0100

    Implement binary search algorithm in Python
    On mobile check later

---
+++ fundamentals/binary_search/Binary_search.py
@@ -0,0 +1,18 @@
+# Welcome to your Python project!
+list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+def bin(list, target):
+    l = 0
+    r = len(list)
+    while l <= r:
+        m = l + (r-l) //2
+        if list[m] == target:
+            return m
+        elif list[m] < target:
+            l = m+1
+        else:
+            r = m-1
+    return None
+
+answer = bin(list, 12)
+print(answer)
