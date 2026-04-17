10572e12a979fd313aea30f24bc79cd34c83cf9d
leshec <103497129+leshec@users.noreply.github.com>
2026-04-18 00:21:25 +0100

    Fix binary search right index calculation
    Off by 1

--- fundamentals/binary_search/Binary_search.py
+++ fundamentals/binary_search/Binary_search.py
@@ -3,7 +3,7 @@ list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 def bin(list, target):
     l = 0
-    r = len(list)
+    r = len(list) - 1
     while l <= r:
         m = l + (r-l) //2
         if list[m] == target:
